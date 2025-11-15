b7fffa44764ce56d85583c79de4b227e48b40c76
Kostya Stern <KostjaStern@gmail.com>
2025-11-15 15:24:57 +0200

    DJV-9: Adding GPL license (#5)
    Co-authored-by: Kostya Stern <kostiantyn.stern@oracle.com>

--- src/test/java/com/sternkn/djvu/file/TestDjVuFile.java
+++ src/test/java/com/sternkn/djvu/file/TestDjVuFile.java
@@ -1,3 +1,20 @@
+/*
+    Copyright (C) 2025 Kostya Stern
+
+    This program is free software; you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by the Free
+    Software Foundation; either version 2 of the License, or (at your option)
+    any later version.
+
+    This program is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+    more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc., 51
+    Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+*/
 package com.sternkn.djvu.file;
 
 import com.sternkn.djvu.file.chunks.Chunk;
@@ -5,7 +22,6 @@ import com.sternkn.djvu.file.chunks.ChunkId;
 import com.sternkn.djvu.file.chunks.SecondaryChunkId;
 import com.sternkn.djvu.file.coders.TestSupport;
 import org.junit.jupiter.api.Test;
-
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
